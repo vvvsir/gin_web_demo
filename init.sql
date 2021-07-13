dfee880d8b448433b9986cb72e4b9f9265d8d976
leo <>
2021-07-13 13:57:01 +0800

    update init.sql script

--- init.sql
+++ init.sql
@@ -1,2 +1,16 @@
 -- create the databases
-CREATE DATABASE IF NOT EXISTS gin_web_demo;
+CREATE DATABASE IF NOT EXISTS gin_web_demo default character set utf8 collate utf8_general_ci;
+USE gin_web_demo;
+create table user
+(
+    id          bigint auto_increment primary key,
+    user_id     bigint                              not null,
+    username    varchar(64)                         not null,
+    password    varchar(64)                         not null,
+    email       varchar(64)                         null,
+    gender      tinyint   default 0                 not null,
+    create_time timestamp default CURRENT_TIMESTAMP null,
+    update_time timestamp default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    constraint idx_user_id unique (user_id),
+    constraint idx_username unique (username)
+) collate = utf8mb4_general_ci;
